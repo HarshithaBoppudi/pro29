1c5878279c2446f22866c27fda92974c70f7e4e6
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2020-07-30 17:37:33 +0530

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -9,16 +9,47 @@ class BaseClass{
         this.width = width;
         this.height = height;
         World.add(world, this.body);
+        this.Visiblity=255;
       }
+
       display(){
-        push();
-       fill("aqua");
-      // var pos =this.body.position;
+       // console.log(this.body.speed);
+        if(this.body.speed<8){
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rect(0, 0, this.width, this.height);
+          pop();
+         
+       }
+        else{
+          
+         // fill("aqua");
+         
+          World.remove(world,this.body);
+          push();
+         
+          this.Visiblity=this.Visiblity-5;
+         /* translate(this.body.position.x, this.body.position.y);
+          rect(0, 0, this.width, this.height);
+     
+         // tint(255,this.visiblity);
+         // (this.body,this.body.position.x,this.body.position.y);*/
+         pop();
+        }
+        
+       //push();
        
+       // var pos =this.body.position;
+      
+        
+      
+        
+         //pop();
+          }
+          score(){
+            if(this.Visiblity<0&&this.Visiblity>-105){
+              score++
+            }
+          }
        
-        translate(this.body.position.x, this.body.position.y);
-        rect(0, 0, this.width, this.height);
-       
-        pop();
-      }
-}
+        }
--- sketch.js
+++ sketch.js
@@ -6,10 +6,11 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var platform;
-//var bird, slingShot;
-
+//var polygon;
+var backgroundImg
+var score=0;
 function preload() {
-   
+   backgroundImg=loadImage("bg.jpg")
 }
 
 function setup(){
@@ -24,7 +25,8 @@ function setup(){
 
 
   //  box1 = new BaseClass(340,170,20,20);
-    box2 = new  BaseClass(200,50,40,40);
+  //fill("pink");
+    box2 = new  Box(200,50,40,40);
    
 
     box3 = new BaseClass(390,170,40,40);
@@ -58,52 +60,101 @@ function setup(){
 }
 
 function draw(){
-   background (0);
+   background (backgroundImg);
+   textSize(30);
+   text("score:"+score,900,50);
     Engine.update(engine);
     strokeWeight(4);
    // box1.display();
     box2.display();
     ground.display();
    
-
+  fill("green");
     box3.display();
+    box3.score();
+
     box4.display();
-   
+    box4.score();
 
     box5.display();
+    box5.score();
+
     box6.display();
+    box6.score();
+
     box7.display();
+    box7.score();
+
     box8.display();
+    box8.score();
+    fill("blue");
     box9.display();
-    box10.display();
-    box11.display();
-    box12.display();
-    box13.display();
-    box14.display();
-    box15.display();
-    box16.display();
-    box17.display();
-    box18.display();
-    box19.display();
-    box20.display();
-    box21.display();
+    box9.score();
     
+    box10.display();
+    box10.score();
+
+    box11.display();
+    box11.score();
+
+    box12.display();
+    box12.score();
+
+    box13.display();
+    box13.score();
+
+    box14.display();
+    box14.score();
+    fill("pink");
+    box15.display();
+    box15.score();
+
+    box16.display();
+    box16.score();
+
+    box17.display();
+    box17.score();
+
+    box18.display();
+    box18.score();
+
+    box19.display();
+    box19.score();
+
+    box20.display();
+    box20.score();
+
+    box21.display();
+    box21.score();
 
     box22.display();
+    box22.score();
+
     box23.display();
+    box23.score();
+    fill("yellow");
     box24.display();
+    box24.score();
+
     box25.display();
+    box25.score();
+
     box26.display();
+    box26.score();
+
     box27.display();
+    box27.score();
+
     box28.display();
+    box28.score();
+
     box29.display();
+    box29.score();
     //bird.display();
     platform.display();
     platform2.display();
     //log6.display();
-  //  slingshot.display();
-    
-       
+    slingshot.display()
 }
 
 function mouseDragged(){
@@ -114,17 +165,9 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
-//function keyPressed(){
-//if(keyCode===32){
+function keyPressed(){
+if(keyCode===32){
 
-   // slingshot.attach(bird.body);
-//}
-
-
-
-
-
-
-
-
- 
+    slingshot.attach(box2.body);
+}
+}
